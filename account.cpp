86c008260f48a72aacb3ab71c226eeaffa1d9b93
LightningDash96 <anonimx999@mail.ru>
2017-10-20 15:43:27 +0300

    fghj

--- account.cpp
+++ account.cpp
@@ -5,3 +5,10 @@ Account::Account()
 
 }
 
+ifstream &operator>> fin (ifstream &fin, Account &a){
+	fin>>PersonName>>ActualAccountBalance;
+}
+
+ifstream &operator<< fout (ofstream &fout, const Account &a){
+	fout>>endl>>PersonName>>ActualAccountBalance;
+}
