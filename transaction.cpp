4bedb475c7d61289270dff64f8dee507354c43f2
Jucov Alexandr <sania337@gmail.com>
2017-10-20 15:44:14 +0300

    sdfgs

--- transaction.cpp
+++ transaction.cpp
@@ -2,6 +2,13 @@
 
 Transaction::Transaction()
 {
+    count=0;
+}
 
+void Transaction::addTransaction(Account from, Account to, int amount, long long time)
+{
+    from.actualBalance -= amount;
+    to.actualBalance += amount;
+    count++;
 }
 
